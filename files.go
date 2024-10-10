591b1d4c2057199dc0f3a0f2410f786dbe2ffeb2
fedorenkodk <fedorenkodk@gmail.com>
2024-10-10 01:00:55 +0100

    Add file writing

--- files.go
+++ files.go
@@ -14,3 +14,19 @@ func printFile(filePath string) {
 
 	fmt.Println(string(content))
 }
+
+func saveResponseToFile(response *[]byte) {
+	const filename = "./resources/response.json"
+
+	prettyJson, err := getPrettyJson(response)
+	if err != nil {
+		fmt.Println("Could not get pretty Json:", err)
+		return
+	}
+
+	err = os.WriteFile(filename, prettyJson.Bytes(), 0644) // Write to file with read/write permissions
+	if err != nil {
+		println("error writing to file", err)
+		return
+	}
+}
